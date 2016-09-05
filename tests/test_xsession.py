da7c0feae20908f365ad1e191be6ef4760195f7f
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 00:12:31 +0100

    Upgraded to Molecule 1.10.3 (#7)
    Fixed flake8 linter warnings.

--- tests/test_xsession.py
+++ tests/test_xsession.py
@@ -14,9 +14,10 @@ def test_xsession_file_permissions(File):
     assert config.group == 'root'
     assert oct(config.mode) == '0644'
 
+
 @pytest.mark.parametrize('expected', [
-    ('XDG_CONFIG_DIRS=/etc/xdg/ansible-default-web-browser:"$XDG_CONFIG_DIRS"'),
-    ('XDG_DATA_DIRS=/etc/xdg/ansible-default-web-browser:"$XDG_DATA_DIRS"')
+    'XDG_CONFIG_DIRS=/etc/xdg/ansible-default-web-browser:"$XDG_CONFIG_DIRS"',
+    'XDG_DATA_DIRS=/etc/xdg/ansible-default-web-browser:"$XDG_DATA_DIRS"'
 ])
 def test_xsession_file(File, expected):
     config = File('/etc/X11/Xsession.d/80-ansible-default-web-browser')
